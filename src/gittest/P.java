42705050209a6f97d96f61047762920a1c2dda30
Gopalsvs33 <165352394+Gopalsvs33@users.noreply.github.com>
2024-03-31 11:57:07 +0530

    Update P.java

--- src/gittest/P.java
+++ src/gittest/P.java
@@ -5,6 +5,7 @@ public class P {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
       System.out.println("God is Great");
+		System.out.println("God is Great333");
 	}
 
 }
